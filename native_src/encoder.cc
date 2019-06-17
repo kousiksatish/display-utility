5ffcb9d3c010b0acb65474e23f1072d8587d77f9
Kousik Satish <kousiksatish@gmail.com>
2019-06-17 14:31:21 +0530

    Replace swscale with yuv conversion function

--- native_src/encoder.cc
+++ native_src/encoder.cc
@@ -25,6 +25,50 @@ namespace remoting
         _rgbData = _screenCapturer->GetDataPointer();
     }
     
+    void Bitmap2Yuv420p_calc2(uint8_t *destination, uint8_t *rgb, size_t width, size_t height)
+    {
+        size_t image_size = width * height;
+        size_t upos = image_size;
+        size_t vpos = upos + upos / 4;
+        size_t i = 0;
+
+        for( size_t line = 0; line < height; ++line )
+        {
+            if( !(line % 2) )
+            {
+                for( size_t x = 0; x < width; x += 2 )
+                {
+                    uint8_t b = rgb[4 * i];
+                    uint8_t g = rgb[4 * i + 1];
+                    uint8_t r = rgb[4 * i + 2];
+
+                    destination[i++] = ((66*r + 129*g + 25*b) >> 8) + 16;
+
+                    destination[upos++] = ((-38*r + -74*g + 112*b) >> 8) + 128;
+                    destination[vpos++] = ((112*r + -94*g + -18*b) >> 8) + 128;
+
+                    b = rgb[4 * i];
+                    g = rgb[4 * i + 1];
+                    r = rgb[4 * i + 2];
+
+                    destination[i++] = ((66*r + 129*g + 25*b) >> 8) + 16;
+                }
+            }
+            else
+            {
+                for( size_t x = 0; x < width; x += 1 )
+                {
+                    uint8_t b = rgb[4 * i];
+                    uint8_t g = rgb[4 * i + 1];
+                    uint8_t r = rgb[4 * i + 2];
+
+                    destination[i++] = ((66*r + 129*g + 25*b) >> 8) + 16;
+                }
+            }
+        }
+    }
+
+
     SwsContext* Encoder::InitializeConverter(int W, int H)
     {
         // Initialise swscale converter
@@ -56,16 +100,18 @@ namespace remoting
         uint8_t* yuv_planes[3] = {yuv_data, yuv_data + W*H, yuv_data + W*H + W*H / 4};
         int yuv_stride[3] = {W, W/2, W/2};
 
-        int returnValue = sws_scale(_swsConverter, rgb_planes, rgb_stride, 0, H, yuv_planes, yuv_stride);
-        if (returnValue == H)
-        {
-            std::cout << "Converted the color space of the image." << std::endl;
-        }
-        else
-        {
-            throw("Failed to convert the color space of the image.");
-        }
+        // int returnValue = sws_scale(_swsConverter, rgb_planes, rgb_stride, 0, H, yuv_planes, yuv_stride);
+        // if (returnValue == H)
+        // {
+        //     std::cout << "Converted the color space of the image." << std::endl;
+        // }
+        // else
+        // {
+        //     throw("Failed to convert the color space of the image.");
+        // }
 
+        Bitmap2Yuv420p_calc2(yuv_data, _rgbData, W, H);
+        
         int luma_size = W * H;
         int chroma_size = luma_size/4;
 
